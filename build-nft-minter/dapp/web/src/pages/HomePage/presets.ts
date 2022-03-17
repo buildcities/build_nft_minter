1973a38ee0e88c7e1d04e0463b8047ddf5c5f23b
Ronald Maduka <rolly.maduk@gmail.com>
2022-03-17 07:10:33 +0100

    FIX: home page edit

--- build-nft-minter/dapp/web/src/pages/HomePage/presets.ts
+++ build-nft-minter/dapp/web/src/pages/HomePage/presets.ts
@@ -1,4 +1,4 @@
 export const MAIN_TEXT_1 = 'build_ asset and token'
 export const MAIN_TEXT_2 = 'Minty'
 export const SUB_TEXT =
-  'Administrative interface for minting and managing build_ assets'
+  'Administrative tools for minting and managing NFT assets'
